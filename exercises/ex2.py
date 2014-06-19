d47d22839afbfc0d007bde1472bbe43c3d5ab615
Nathan Files <nfiles2009@gmail.com>
2014-06-19 15:15:50 -0400

    Exercise 2

---
+++ exercises/ex2.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+# A Comment, .....
+# Anything after the # is ignored by python.
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a pieve of code:
+# print "This won't run."
+
+print "This will run."
